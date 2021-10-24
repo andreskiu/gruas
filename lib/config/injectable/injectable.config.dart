be74d4f8b33bd90dd0ba6f4f72e6aa2a777561e5
Andres <andreskiu@hotmail.com>
2021-10-24 19:00:17 -0300

    libraries updated
    # Conflicts:
    #	lib/config/injectable/injectable.config.dart
    #	lib/presentation/core/routes/app_router.gr.dart
    #	pubspec.lock

--- lib/config/injectable/injectable.config.dart
+++ lib/config/injectable/injectable.config.dart
@@ -7,26 +7,27 @@
 import 'package:dio/dio.dart' as _i4;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:shared_preferences/shared_preferences.dart' as _i17;
+import 'package:shared_preferences/shared_preferences.dart' as _i18;
 
 import '../../application/auth/auth_state.dart' as _i3;
-import '../../application/auth/login_page_state.dart' as _i27;
+import '../../application/auth/login_page_state.dart' as _i28;
 import '../../application/grua/grua_service_state.dart' as _i10;
-import '../../domain/auth/services/auth_service.dart' as _i21;
-import '../../domain/auth/use_cases/get_user_logged_in.dart' as _i23;
-import '../../domain/auth/use_cases/get_user_remembered.dart' as _i24;
-import '../../domain/auth/use_cases/login.dart' as _i25;
-import '../../domain/auth/use_cases/logout.dart' as _i26;
+import '../../domain/auth/services/auth_service.dart' as _i22;
+import '../../domain/auth/use_cases/get_user_logged_in.dart' as _i24;
+import '../../domain/auth/use_cases/get_user_remembered.dart' as _i25;
+import '../../domain/auth/use_cases/login.dart' as _i26;
+import '../../domain/auth/use_cases/logout.dart' as _i27;
 import '../../domain/grua/services/grua_service.dart' as _i15;
-import '../../domain/grua/use_cases/get_services.dart' as _i18;
+import '../../domain/grua/use_cases/get_services.dart' as _i19;
+import '../../domain/grua/use_cases/save_service.dart' as _i17;
 import '../../infrastructure/auth/repositories/demo_auth_repository.dart'
     as _i12;
 import '../../infrastructure/auth/repositories/shared_preferences_auth_repository.dart'
-    as _i20;
-import '../../infrastructure/auth/services/auth_service.dart' as _i22;
+    as _i21;
+import '../../infrastructure/auth/services/auth_service.dart' as _i23;
 import '../../infrastructure/auth/services/i_auth_repository.dart' as _i11;
 import '../../infrastructure/auth/services/i_auth_storage_repository.dart'
-    as _i19;
+    as _i20;
 import '../../infrastructure/grua/repositories/grua_demo_repository.dart'
     as _i14;
 import '../../infrastructure/grua/services/grua_service.dart' as _i16;
@@ -37,7 +38,7 @@ import '../environments/environment_demo.dart' as _i6;
 import '../environments/environment_dev.dart' as _i7;
 import '../environments/environment_prod.dart' as _i8;
 import '../environments/environment_qa.dart' as _i9;
-import 'injectable.dart' as _i28;
+import 'injectable.dart' as _i29;
 
 const String _demo = 'demo';
 const String _dev = 'dev';
@@ -69,29 +70,31 @@ Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
       registerFor: {_demo});
   gh.lazySingleton<_i15.IGruaService>(
       () => _i16.GruaServiceImpl(repository: get<_i13.IGruaDataRepository>()));
-  await gh.factoryAsync<_i17.SharedPreferences>(() => registerModule.prefs,
+  gh.lazySingleton<_i17.SaveServicesUseCase>(
+      () => _i17.SaveServicesUseCase(get<_i15.IGruaService>()));
+  await gh.factoryAsync<_i18.SharedPreferences>(() => registerModule.prefs,
       preResolve: true);
-  gh.lazySingleton<_i18.GetServicesUseCase>(
-      () => _i18.GetServicesUseCase(get<_i15.IGruaService>()));
-  gh.lazySingleton<_i19.IAuthStorageRepository>(() =>
-      _i20.SharedPreferencesAuthRepository(
-          pref: get<_i17.SharedPreferences>()));
-  gh.lazySingleton<_i21.AuthService>(() => _i22.AuthServiceImpl(
+  gh.lazySingleton<_i19.GetServicesUseCase>(
+      () => _i19.GetServicesUseCase(get<_i15.IGruaService>()));
+  gh.lazySingleton<_i20.IAuthStorageRepository>(() =>
+      _i21.SharedPreferencesAuthRepository(
+          pref: get<_i18.SharedPreferences>()));
+  gh.lazySingleton<_i22.AuthService>(() => _i23.AuthServiceImpl(
       repository: get<_i11.IAuthDataRepository>(),
-      localStorage: get<_i19.IAuthStorageRepository>()));
-  gh.lazySingleton<_i23.GetUserLoggedInUseCase>(
-      () => _i23.GetUserLoggedInUseCase(get<_i21.AuthService>()));
-  gh.lazySingleton<_i24.GetUserRememberedUseCase>(
-      () => _i24.GetUserRememberedUseCase(get<_i21.AuthService>()));
-  gh.lazySingleton<_i25.LoginUseCase>(
-      () => _i25.LoginUseCase(get<_i21.AuthService>()));
-  gh.lazySingleton<_i26.LogoutUseCase>(
-      () => _i26.LogoutUseCase(get<_i21.AuthService>()));
-  gh.factoryAsync<_i27.LoginPageState>(() async => _i27.LoginPageState(
-      loginUseCase: get<_i25.LoginUseCase>(),
+      localStorage: get<_i20.IAuthStorageRepository>()));
+  gh.lazySingleton<_i24.GetUserLoggedInUseCase>(
+      () => _i24.GetUserLoggedInUseCase(get<_i22.AuthService>()));
+  gh.lazySingleton<_i25.GetUserRememberedUseCase>(
+      () => _i25.GetUserRememberedUseCase(get<_i22.AuthService>()));
+  gh.lazySingleton<_i26.LoginUseCase>(
+      () => _i26.LoginUseCase(get<_i22.AuthService>()));
+  gh.lazySingleton<_i27.LogoutUseCase>(
+      () => _i27.LogoutUseCase(get<_i22.AuthService>()));
+  gh.factoryAsync<_i28.LoginPageState>(() async => _i28.LoginPageState(
+      loginUseCase: get<_i26.LoginUseCase>(),
       authState: await get.getAsync<_i3.AuthState>(),
-      getUserRememberedUseCase: get<_i24.GetUserRememberedUseCase>()));
+      getUserRememberedUseCase: get<_i25.GetUserRememberedUseCase>()));
   return get;
 }
 
-class _$RegisterModule extends _i28.RegisterModule {}
+class _$RegisterModule extends _i29.RegisterModule {}
