6f29f868ba29134f5dd9705991627af9088968ee
Andres <andreskiu@hotmail.com>
2022-01-26 21:28:08 -0300

    make the route report more accurate

--- lib/config/maps/constants.dart
+++ lib/config/maps/constants.dart
@@ -1,6 +1,6 @@
 abstract class MapConstants {
 // tiempo de revision de la ubicacion de la persona en milisegundos
-  static const LocationIntervalUpdate = 30000;
+  static const LocationIntervalUpdate = 15000;
   // distancia minima para actualizar la ubicacion de la persona en metros
-  static const LocationDistanceUpdate = 100.0;
+  static const LocationDistanceUpdate = 50.0;
 }
